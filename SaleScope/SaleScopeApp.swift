2c37d307d005add20bd57a3186a96a085e231a2a
Pedro Prado <pedroprado1418@gmail.com>
2024-04-30 11:07:22 -0600

    Initial Commit

---
+++ SaleScope/SaleScopeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaleScopeApp.swift
+//  SaleScope
+//
+//  Created by Pedro Prado on 30/04/24.
+//
+
+import SwiftUI
+
+@main
+struct SaleScopeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
